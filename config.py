6d64b85643a387e3c9ed4fc822a1da8261079e5a
sebastienlanglois <sebastien.langlois@polymtl.ca>
2020-01-28 20:23:40 -0500

    initial commit

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+    ADMINS = ['your-email@example.com']
+    LANGUAGES = ['en']
+    REDIS_URL = os.environ.get('REDIS_URL') or 'redis://'
