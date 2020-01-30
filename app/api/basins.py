920d5bc38f567c4505ac0c59be494f1e4fef033b
slangloislanglois <sebastien.langlois@polymtl.ca>
2020-01-30 11:10:24 -0500

    api sqp query

--- app/api/basins.py
+++ app/api/basins.py
@@ -29,12 +29,19 @@ def meta_ts():
     return df.to_json(orient="table")
 
 
-# @bp.route('/basins', methods=['GET'])
-# def basins():
-#     engine = create_engine(Config.SQLALCHEMY_DATABASE_URI,
-#                            echo=False)
-#     sql = """
-#     SELECT * FROM basins
-#     """
-#     df = pd.read_sql(sql,con=engine)
-#     return df.to_json(orient="records")
+@bp.route('/query', methods=['GET', 'POST'])
+def query():
+    engine = create_engine(Config.SQLALCHEMY_DATABASE_URI,
+                           echo=False)
+    # sql = """
+    # SELECT * FROM basins
+    # """
+    sql = request.args.get('query', default=1, type=str)
+    print(sql)
+    try:
+        df = pd.read_sql(sql, con=engine)
+        output = df.to_json(orient="table")
+    except RuntimeError:
+        output = f'Query not valid'
+
+    return output
