fc7e01d145d7ca2a08c95a627a24b816ea9e2c8a
slangloislanglois <sebastien.langlois@polymtl.ca>
2020-02-14 14:53:46 -0500

    api sqp query

--- app/models.py
+++ app/models.py
@@ -1,46 +1,144 @@
 from app import db
-
-# Base = declarative_base()
-# engine = create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/hydrodatahub', echo=True)
-#
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
 
 
-class basins(db.Model):
-    id_point = db.Column('id_point', db.Integer, primary_key=True)
-    numero_station = db.Column('station_number', db.String)
-    nom_station = db.Column('station_name', db.String)
-    nom_equiv = db.Column('equivalent_name', db.String)
-    province = db.Column('province', db.String)
-    regime = db.Column('regulation', db.String)
-    superficie = db.Column('drainage_area', db.Integer)
-    latitude = db.Column('latitude', db.REAL)
-    longitude = db.Column('longitude', db.REAL)
+Base = declarative_base()
+engine = create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/hydrodatahub_design', echo=True)
+
+
+class region(db.Model):
+    __tablename__ = 'region'
+    __table_args__ = {'extend_existing': True}
+    id_region = db.Column('id_region', db.Integer, primary_key=True)
+    nom_region = db.Column('nom_region', db.String)
+    value = db.Column('value', db.REAL)
 
     def __repr__(self):
-        return '<basins {}>'.format(self.id_point)
+        return '<date {}>'.format(self.value)
 
 
-class meta_ts(db.Model):
-    id_serie = db.Column('id_time_serie', db.Integer, primary_key=True)
-    id_point = db.Column('id_point', db.Integer, db.ForeignKey(basins.id_point))
-    type_serie = db.Column('data_type', db.String)
-    pas_de_temps = db.Column('time_step', db.String)
+class station(db.Model):
+    __tablename__ = 'station'
+    __table_args__ = {'extend_existing': True}
+    id_station = db.Column('id_station', db.Integer, primary_key=True)
+    numero_station = db.Column('numero_station', db.String)
+    province = db.Column('province', db.String)
+    latitude = db.Column('latitude', db.REAL)
+    longitude = db.Column('longitude', db.REAL)
+    id_region = db.Column('id_region', db.Integer, db.ForeignKey(region.id_region))
+
+    def __repr__(self):
+        return '<amenagement {}>'.format(self.nom_amenagement)
+
+
+class bassin(db.Model):
+    __tablename__ = 'bassin'
+    __table_args__ = {'extend_existing': True}
+    id_bassins = db.Column('id_bassins', db.Integer, primary_key=True)
+    numero_station = db.Column('numero_station', db.String)
+    nom_station = db.Column('nom_station', db.String)
+    nom_equiv = db.Column('nom_equivalent', db.String)
+    province = db.Column('province', db.String)
+    regime = db.Column('regularisation', db.String)
+    superficie = db.Column('superficie', db.Integer)
+    latitude = db.Column('latitude', db.REAL)
+    longitude = db.Column('longitude', db.REAL)
+    id_region = db.Column('id_region', db.Integer, db.ForeignKey(region.id_region))
+
+    def __repr__(self):
+        return '<basins {}>'.format(self.id_bassins)
+
+
+class amenagement(db.Model):
+    __tablename__ = 'amenagement'
+    __table_args__ = {'extend_existing': True}
+    id_amenagement = db.Column('id_amenagement', db.Integer, primary_key=True)
+    nom_amenagement = db.Column('nom_amenagement', db.String)
+    id_bassins = db.Column('id_bassins', db.Integer, db.ForeignKey(bassin.id_bassins))
+
+    def __repr__(self):
+        return '<amenagement {}>'.format(self.nom_amenagement)
+
+
+class reservoir(db.Model):
+    __tablename__ = 'reservoir'
+    __table_args__ = {'extend_existing': True}
+    id_reservoir = db.Column('id_reservoir', db.Integer, primary_key=True)
+    nom_amenagement = db.Column('nom_amenagement', db.String)
+    id_bassins = db.Column('id_bassins', db.Integer, db.ForeignKey(bassin.id_bassins))
+    id_amenagement = db.Column('id_amenagement', db.Integer, db.ForeignKey(amenagement.id_amenagement))
+
+    def __repr__(self):
+        return '<reservoirs {}>'.format(self.id_reservoir)
+
+
+class evacuateur(db.Model):
+    __tablename__ = 'evacuateur'
+    __table_args__ = {'extend_existing': True}
+    id_evacuateur = db.Column('id_evacuateur', db.Integer, primary_key=True)
+    nom_amenagement = db.Column('nom_evacuateur', db.String)
+    id_reservoir = db.Column('id_reservoir', db.Integer, db.ForeignKey(reservoir.id_reservoir))
+    id_amenagement = db.Column('id_amenagement', db.Integer, db.ForeignKey(amenagement.id_amenagement))
+
+    def __repr__(self):
+        return '<evacuateur {}>'.format(self.id_evacuateur)
+
+
+class centrale(db.Model):
+    __tablename__ = 'centrale'
+    __table_args__ = {'extend_existing': True}
+    id_centrale = db.Column('id_centrale', db.Integer, primary_key=True)
+    nom_centrale = db.Column('nom_centrale', db.String)
+    id_reservoir = db.Column('id_reservoir', db.Integer, db.ForeignKey(reservoir.id_reservoir))
+    id_amenagement = db.Column('id_amenagement', db.Integer, db.ForeignKey(amenagement.id_amenagement))
+
+    def __repr__(self):
+        return '<centrale {}>'.format(self.id_centrale)
+
+
+class meta_series(db.Model):
+    __tablename__ = 'meta_series'
+    __table_args__ = {'extend_existing': True}
+    id_serie = db.Column('id', db.Integer, primary_key=True)
+    id_point = db.Column('id_bassins', db.Integer, db.ForeignKey(bassin.id_bassins))
+    type_serie = db.Column('type_serie', db.String)
+    pas_de_temps = db.Column('pas_de_temps', db.String)
     aggregation = db.Column('aggregation', db.String)
-    unite = db.Column('units', db.String)
-    date_debut = db.Column('start_date', db.DateTime(timezone=True))
-    date_fin = db.Column('end_date', db.DateTime(timezone=True))
+    unite = db.Column('unites', db.String)
+    date_debut = db.Column('date_debut', db.DateTime(timezone=True))
+    date_fin = db.Column('date_fin', db.DateTime(timezone=True))
+    source = db.Column('source', db.String)
+
+    def __repr__(self):
+        return '<meta_ts {}>'.format(self.id_serie)
+
+class meta_series(db.Model):
+    __tablename__ = 'meta_series'
+    __table_args__ = {'extend_existing': True}
+    id_serie = db.Column('id', db.Integer, primary_key=True)
+    id_point = db.Column('id_bassins', db.Integer, db.ForeignKey(bassin.id_bassins))
+    type_serie = db.Column('type_serie', db.String)
+    pas_de_temps = db.Column('pas_de_temps', db.String)
+    aggregation = db.Column('aggregation', db.String)
+    unite = db.Column('unites', db.String)
+    date_debut = db.Column('date_debut', db.DateTime(timezone=True))
+    date_fin = db.Column('date_fin', db.DateTime(timezone=True))
     source = db.Column('source', db.String)
 
     def __repr__(self):
         return '<meta_ts {}>'.format(self.id_serie)
 
 
-class don_ts(db.Model):
-    id_serie = db.Column('id_time_serie', db.Integer, db.ForeignKey(meta_ts.id_serie), primary_key=True)
+class don_series(db.Model):
+    __tablename__ = 'don_series'
+    __table_args__ = {'extend_existing': True}
+    id_serie = db.Column('id', db.Integer, db.ForeignKey(meta_series.id_serie), primary_key=True)
     date = db.Column('date', db.DateTime(timezone=True), primary_key=True)
     value = db.Column('value', db.REAL)
 
     def __repr__(self):
         return '<date {}>'.format(self.value)
 
-# Base.metadata.create_all(engine)
+
+Base.metadata.create_all(engine)
